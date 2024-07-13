545365b59f0f56b025747215a3aa9743d0015b28
Rishi <Rishi.com>
2024-07-13 21:38:46 +0530

    a lotof things are good

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ document.getElementById('weatherForm').addEventListener('submit', function(event
     event.preventDefault();
 
     const city = document.getElementById('city').value;
-    const apiKey = '68f193929564427da70184519241107';
+    const apiKey = 'd2cf7cdb53fb46fc907152419241307';
     const url = `http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${city}`;
 
     fetch(url)
@@ -10,7 +10,15 @@ document.getElementById('weatherForm').addEventListener('submit', function(event
             return response.json();
         })
         .then(data => {
-            document.getElementById('feelslike_c').textContent = data.current.feelslike_c;            console.log(data);
+            document.getElementById('feelslike_c').textContent = data.current.feelslike_c;
+            document.getElementById('dewpoint_c').textContent = data.current.dewpoint_c;
+            document.getElementById('humidity').textContent = data.current.humidity;
+            document.getElementById('wind_kph').textContent = data.current.wind_kph;
+            document.getElementById('wind_dir').textContent = data.current.wind_dir;
+            document.getElementById('localtime').textContent = data.location.localtime;
+            document.getElementById('region').textContent = data.location.region;
+            document.getElementById('pressure_mb').textContent = data.current.pressure_mb;
+            console.log(data);
         })
         .catch(error => {
             console.error('Problem', error);
