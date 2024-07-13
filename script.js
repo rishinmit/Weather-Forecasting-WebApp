72739026efba1ece6526aca465c82f17c2221bd9
Rishi <Rishi.com>
2024-07-13 20:52:39 +0530

    latest2

--- script.js
+++ script.js
@@ -1,22 +1,18 @@
-const url = 'http://api.weatherapi.com/v1/current.json?key=68f193929564427da70184519241107&q=Patna';
-const options = {
-	method: 'GET',
-	headers: {
-		'x-rapidapi-key': '68f193929564427da70184519241107',
-		'x-rapidapi-host': 'http://api.weatherapi.com/'
-	}
-};
+document.getElementById('weatherForm').addEventListener('submit', function(event) {
+    event.preventDefault();
 
-// try {
-// 	const response = await fetch(url, options);
-// 	const result = await response.text();
-// 	console.log(result);
-// } catch (error) {
-// 	console.error(error);
-// }
-fetch(url, options)
-    .then(response => response.json())
-    .then(response => {
-        
-        console.log(response)})
-    .catch(error => console.error(error));
+    const city = document.getElementById('city').value;
+    const apiKey = '68f193929564427da70184519241107';
+    const url = `http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${city}`;
+
+    fetch(url)
+        .then(response => {
+            return response.json();
+        })
+        .then(data => {
+            document.getElementById('feelslike_c').textContent = data.current.feelslike_c;            console.log(data);
+        })
+        .catch(error => {
+            console.error('Problem', error);
+        });
+});
